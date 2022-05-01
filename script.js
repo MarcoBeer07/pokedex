4b3e2a831bc70f560473ef1966aab20420b416f3
Marco <marco.beer93@web.de>
2022-05-01 17:31:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+async function loadPokemon() {
+    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let response = await fetch(url);
+    let responseAsJson = await response.json();
+
+}

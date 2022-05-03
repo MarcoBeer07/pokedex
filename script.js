d5cc77508cf80b18d7523f4328f3972eed3c314c
Marco <marco.beer93@web.de>
2022-05-03 20:45:11 +0200

    added container for Pokemon

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+let currentPokemon;
+
 async function loadPokemon() {
     let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
     let response = await fetch(url);
-    let responseAsJson = await response.json();
+    currentPokemon = await response.json();
+    renderPokemonInfo();
+}
 
+function renderPokemonInfo() {
+    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
 }

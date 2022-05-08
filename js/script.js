6b2c12330076184599edb8bdeb96a0b6a0661568
Marco <marco.beer93@web.de>
2022-05-08 12:56:56 +0200

    added big View onclick

--- js/script.js
+++ js/script.js
@@ -1,19 +1,76 @@
 let currentPokemon;
+let allPokemons = [];
+
+function init() {
+    loadPokemon();
+
+}
+
 async function loadPokemon() {
-    for (let i = 1; i < 151; i++) {
+    for (let i = 1; i < 10; i++) {
 
         let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         let response = await fetch(url);
         currentPokemon = await response.json();
+        allPokemons.push(currentPokemon);
         renderPokemonBox(i);
     }
 }
 
+
 function renderPokemonBox(i) {
     let pokemonMainBox = document.getElementById('pokemonMainBox')
-    let type = currentPokemon['types'][0]['type']['name'];
-
     pokemonMainBox.innerHTML += renderPokemonHTML(i);
+    setRightColorNormalView(i)
+}
+
+
+function renderPokemoBigView(i) {
+    document.getElementById('pokemonBigViewBox').classList.remove('d-none')
+    let bigViewImage = document.getElementById('pokemonBigView');
+    let bigViewInfo = document.getElementById('pokemonBigViewInfo');
+
+    bigViewImage.innerHTML = '';
+    bigViewInfo.innerHTML = '';
+
+    setRightColorBigView(i)
+
+    bigViewImage.innerHTML += renderPokemonBigViewHTML(i);
+    bigViewInfo.innerHTML += renderPokemonBigViewInfoHTML(i);
+}
+
+function hideBigView() {
+    document.getElementById('pokemonBigViewBox').classList.add('d-none')
+}
+
+
+
+function setRightColorBigView(i) {
+    let type = allPokemons[i - 1]['types'][0]['type']['name'];
+
+    if (type == 'grass') {
+        document.getElementById("pokemonBigView").style.backgroundColor = '#99C262'
+    } else if (type == 'fire') {
+        document.getElementById("pokemonBigView").style.backgroundColor = '#FF3030'
+    } else if (type == 'water') {
+        document.getElementById("pokemonBigView").style.backgroundColor = '#1C86EE'
+    } else if (type == 'poison') {
+        document.getElementById("pokemonBigView").style.backgroundColor = '#8a2be2'
+    } else if (type == 'bug') {
+        document.getElementById("pokemonBigView").style.backgroundColor = '#54FF9F'
+    } else if (type == 'normal') {
+        document.getElementById("pokemonBigView").style.backgroundColor = '#D2691E'
+    } else if (type == 'electric') {
+        document.getElementById("pokemonBigView").style.backgroundColor = '#B3EE3A'
+    } else if (type == 'fairy') {
+        document.getElementById("pokemonBigView").style.backgroundColor = '#C1CDCD'
+    } else if (type == 'ground') {
+        document.getElementById("pokemonBigView").style.backgroundColor = '#8B7E66'
+    }
+}
+
+function setRightColorNormalView(i) {
+    let type = currentPokemon['types'][0]['type']['name'];
 
     if (type == 'grass') {
         document.getElementById("pokemonBox" + i).style.backgroundColor = '#99C262'
@@ -34,28 +91,4 @@ function renderPokemonBox(i) {
     } else if (type == 'ground') {
         document.getElementById("pokemonBox" + i).style.backgroundColor = '#8B7E66'
     }
-}
-
-function pokemonBigView() {
-
-}
-
-async function renderPokemoBigView(i) {
-
-    document.getElementById('pokemonBigViewBox').classList.remove('d-none')
-    let bigViewImage = document.getElementById('pokemonBigView');
-    let bigViewInfo = document.getElementById('pokemonBigViewInfo');
-
-    bigViewImage.innerHTML = '';
-    bigViewInfo.innerHTML = '';
-
-
-    bigViewImage.innerHTML += renderPokemonBigViewHTML(i);
-    bigViewInfo.innerHTML += renderPokemonBigViewInfoHTML(i);
-
-}
-
-function hideBigView() {
-    document.getElementById('pokemonBigViewBox').classList.add('d-none')
-
 }
--- js/template.js
+++ js/template.js
@@ -11,9 +11,9 @@ function renderPokemonHTML(i) {
 function renderPokemonBigViewHTML(i) {
     return `
         <div>
-            <h1>${currentPokemon['name']}</h1>
-            <h3>${currentPokemon['types'][0]['type']['name']}<h3>
-            <img class="pokemon-big-view-img" src=${currentPokemon['sprites']['other']['dream_world']['front_default']}>
+            <h1>${allPokemons[i-1]['name']}</h1>
+            <h3>${allPokemons[i-1]['types'][0]['type']['name']}<h3>
+            <img class="pokemon-big-view-img" src=${allPokemons[i-1]['sprites']['other']['dream_world']['front_default']}>
         </div>
         `
 }
@@ -22,8 +22,17 @@ function renderPokemonBigViewInfoHTML(i) {
     return `
         <div>
             <h1>About</h1>
-            <h3>${currentPokemon['types'][0]['type']['name']}<h3>
+            <p>Height: ${allPokemons[i-1]['height']}<p>
+            <p>Weight: ${allPokemons[i-1]['weight']}<p>
+            <p>Abilitie: ${allPokemons[i-1]['abilities'][0]['ability']['name']}<p>
             <h1>Base Stats</h1>
+            <p>HP: ${allPokemons[i-1]['height']}<p>
+            <p>Attack: ${allPokemons[i-1]['height']}<p>
+            <p>Defense: ${allPokemons[i-1]['height']}<p>
+            <p>Special-Attack: ${allPokemons[i-1]['height']}<p>
+            <p>Special-Defense: ${allPokemons[i-1]['height']}<p>
+            <p>Speed: ${allPokemons[i-1]['height']}<p>
+
         </div>
         `
 }
